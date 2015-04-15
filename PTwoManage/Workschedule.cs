ce74f86108b709c43a7dff9bb31659ea018758ce
ronnilenvighansen <ronnilenvighansen@gmail.com>
2015-04-15 15:01:28 +0200

    Til smed

--- PTwoManage/Workschedule.cs
+++ PTwoManage/Workschedule.cs
@@ -10,13 +10,51 @@ namespace PTwoManage
     class Workschedule
     {
         
-       
         public static void NewCalendar()
         {
             DateTime dt = new DateTime(2015, 05, 25, new GregorianCalendar());
             Calendar cal = CultureInfo.InvariantCulture.Calendar;
             dt = cal.AddYears(dt, 2);
             Console.WriteLine(cal.GetYear(dt));
+        }
+
+        enum Weekdays 
+        {
+            Monday,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+
+        public class Week
+        {
+            DateTime start = new DateTime(2008, 5, 1, 8, 30, 0);
+            DateTime slut = new DateTime(2008, 5, 1, 16, 0, 0);
+            public int weeknumber;
+            day test = new day(Weekdays.Friday, 2, false, start, slut);
+            List<day> w = new List<day>();
+            
+        }
+
+        public class day
+        {
+            public Weekdays weekday;
+            public int numberOfShifts;
+            public bool holiday;
+            public DateTime openingHour;
+            public DateTime closingHour;
+       
+            public day (Weekdays dayInWeek, int shiftsInDay, bool isHoliday, DateTime opening, DateTime closing)
+            {
+                weekday = dayInWeek;
+                numberOfShifts = shiftsInDay;
+                holiday = isHoliday;
+                openingHour = opening;
+                closingHour = closing;
+            }
         }
     }
     
