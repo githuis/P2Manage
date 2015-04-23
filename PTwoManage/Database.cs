ccefb49e80584a2be5d9a876738acbcf74feba22
Hede93 <hede93@live.dk>
2015-04-23 10:20:38 +0200

    sry

--- PTwoManage/Database.cs
+++ PTwoManage/Database.cs
@@ -27,49 +27,22 @@ namespace PTwoManage
             string file = AppDomain.CurrentDomain.BaseDirectory + "MyDatabase.sqlite";
             if (!File.Exists(file))
                 SQLiteConnection.CreateFile("MyDatabase.sqlite");
+            
             m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
             m_dbConnection.Open();
 
-            Execute("CREATE TABLE userTable (id int NOT NULL, username VARCHAR(50), password VARCHAR(50), name VARCHAR(50),cprNumber INT, phone INT, email VARCHAR(50))");
+            
+            Execute("CREATE TABLE IF NOT EXISTS userTable (id int NOT NULL, username VARCHAR(50), password VARCHAR(50), name VARCHAR(50),cprNumber VARCHAR(50), phone VARCHAR(50), email VARCHAR(50))"); 
+            
             Read("SELECT * FROM userTable WHERE id=2", "id", "name");
             readInfo.ForEach(Console.WriteLine);
         }
-
-        
-
-        /*public Database()
-        {
-            readInfo = new List<string>();
-
-            string file = AppDomain.CurrentDomain.BaseDirectory + "MyDatabase.sqlite";
-            if (!File.Exists(file))
-                SQLiteConnection.CreateFile("MyDatabase.sqlite");
-            m_dbConnection = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
-            m_dbConnection.Open();
-                                                
-            //Execute("CREATE TABLE userTable (id int NOT NULL, username VARCHAR(50), password VARCHAR(50), name VARCHAR(50),cprNumber INT, phone INT, email VARCHAR(50))");
-            Read("SELECT * FROM userTable WHERE id=2", "id", "name");
-            readInfo.ForEach(Console.WriteLine);
-        }*/
 
         public void Execute(string sql)
         {
             SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
             command.ExecuteNonQuery();
         }
-
-        /*public void UsertableRead(string sql)
-        {
-            string s;
-            readInfo.Clear();
-            SQLiteCommand command = new SQLiteCommand(sql, m_dbConnection);
-            SQLiteDataReader reader = command.ExecuteReader();
-            while (reader.Read())
-            {
-                s = reader["id"] + " , " + reader["username"] + " , " + reader["password"] + " , " + reader["name"] + " , " + reader["cprNumber"] + " , " + reader["phone"] + " , " + reader["email"];
-                readInfo.Add(s);
-            }
-        }*/
 
         public void Read(string sql, params string[] elements)
         {
@@ -86,13 +59,6 @@ namespace PTwoManage
                 readInfo.Add(s);
 
             }
-        }
-
-        public void CreateTable()
-        {
-            
-
-        
         }
     }
 }
