c26ac221620d9eba037837a920dc33386ea5301d
githuis <zhex@live.dk>
2015-05-03 15:01:21 +0200

    Added company login, fixed cpr overflow

--- PTwoManage/Core.cs
+++ PTwoManage/Core.cs
@@ -28,7 +28,8 @@ namespace PTwoManage
             _allUsers = new List<User>();
             _allTemplates = new List<ShiftTemplate>();
             _allRequests = new List<UserFreeRequest>();
-             _info = new List<string>();
+            _info = new List<string>();
+            _allShifts = new List<Shift>();
             
             string sql = "SELECT * FROM userTable";
             Database.Instance.Read(sql, ref _info, Database.Instance.userTableColumns);
@@ -71,27 +72,17 @@ namespace PTwoManage
 
         }
        
-       /*public void Run()
+       public void Run()
         {
-            User bruger = new User(1,"lucrah2", "1234", "Jens Petersen", "564455648", "88888888", "jgdagmailcom", _allTags);
 
-            DateTime start = new DateTime(2015, 04, 20, 15, 30, 00);
-            DateTime end = new DateTime(2015, 04, 20, 18, 00, 00);
-            DateTime end2 = new DateTime(2015, 04, 20, 19, 00,00);
-
-            Shift vagt = new Shift(bruger, start, end);
-
-            Console.WriteLine(vagt.ToString());
-            _allShifts.Add(vagt);
-            _allShifts.Add(vagt);
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 21, 15, 30, 00), new DateTime(2015, 04, 21, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 22, 15, 30, 00), new DateTime(2015, 04, 22, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 23, 15, 30, 00), new DateTime(2015, 04, 23, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 24, 15, 30, 00), new DateTime(2015, 04, 24, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 25, 15, 30, 00), new DateTime(2015, 04, 25, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 26, 15, 30, 00), new DateTime(2015, 04, 26, 16, 30, 00)));
-            _allShifts.Add(new Shift(bruger, new DateTime(2015, 04, 27, 15, 30, 00), new DateTime(2015, 04, 27, 16, 30, 00)));
-        } */
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 21, 15, 30, 00), new DateTime(2015, 04, 21, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 22, 15, 30, 00), new DateTime(2015, 04, 22, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 23, 15, 30, 00), new DateTime(2015, 04, 23, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 24, 15, 30, 00), new DateTime(2015, 04, 24, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 25, 15, 30, 00), new DateTime(2015, 04, 25, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 26, 15, 30, 00), new DateTime(2015, 04, 26, 16, 30, 00), "Åbner", 10, 2));
+            _allShifts.Add(new Shift("", new DateTime(2015, 04, 27, 15, 30, 00), new DateTime(2015, 04, 27, 16, 30, 00), "Åbner", 10, 2));
+        }
             
         
         public List<User> GetAllUsers()
