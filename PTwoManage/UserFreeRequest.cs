6d287bcd2669e65a418cf7017f5bfb457d2d7169
North-Soul <smed72@live.dk>
2015-05-15 23:37:03 +0200

    Optimized generator + added user equals

--- PTwoManage/UserFreeRequest.cs
+++ PTwoManage/UserFreeRequest.cs
@@ -11,29 +11,26 @@ namespace PTwoManage
         private DateTime _startTime;
         private DateTime _endTime;
         private string _message;
-        private string _userName;
+        private User _user;
 
-        public string UserName
+        public User User
         {
-            get { return _userName; }
-            set { _userName = value; }
+            get { return _user; }
+            set { _user = value; }
         }
         
-
         public string Message
         {
             get { return _message; }
             set { _message = value; }
         }
         
-
         public DateTime Endtime
         {
             get { return _endTime; }
             set { _endTime = value; }
         }
         
-
         public DateTime StartTime
         {
             get { return _startTime; }
@@ -45,13 +42,13 @@ namespace PTwoManage
             _startTime = start;
             _endTime = end;
             _message = message;
-            _userName = userName;
+            _user = User.GetUserByName(userName);
         }
 
         public void SaveUserRequest()
         {
             UserFreeRequest request = this;
-            string sql = "INSERT INTO FreeRequestTable (start, end, text, userID) values ('" + request._startTime.ToString() + "', '" + request._endTime.ToString() + "', '" + request._message + "', '" + request._userName + "')";
+            string sql = "INSERT INTO FreeRequestTable (start, end, text, userID) values ('" + request._startTime.ToString() + "', '" + request._endTime.ToString() + "', '" + request._message + "', '" + request.User.Id + "')";
             Database.Instance.Execute(sql);
             Core.Instance.GetAllFreeRequests().Add(this);
         }
